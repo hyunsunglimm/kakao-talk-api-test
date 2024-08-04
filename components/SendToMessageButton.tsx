eebebb9689fe5d212605ddd601132ce836c0133f
hyunsunglimm <gustjd3834@naver.com>
2024-08-04 21:04:46 +0900

    환경변수 설정

--- components/SendToMessageButton.tsx
+++ components/SendToMessageButton.tsx
@@ -3,6 +3,7 @@
 export default function SendToMessageButton() {
   const sendToMe = () => {
     const { Kakao } = window;
+
     Kakao.API.request({
       url: "/v2/api/talk/memo/default/send",
       data: {
@@ -10,8 +11,8 @@ export default function SendToMessageButton() {
           object_type: "text",
           text: "기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.",
           link: {
-            mobile_web_url: "http://localhost:3000",
-            web_url: "http://localhost:3000",
+            mobile_web_url: process.env.NEXT_PUBLIC_BASE_URL,
+            web_url: process.env.NEXT_PUBLIC_BASE_URL,
           },
         },
       },
