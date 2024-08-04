8982ae5c5c2b33253bc0cf157ba62ddfd33e8374
hyunsunglimm <gustjd3834@naver.com>
2024-08-04 21:40:55 +0900

    REST API 버전

--- components/SendToMessageButton.tsx
+++ components/SendToMessageButton.tsx
@@ -7,29 +7,42 @@ type SendToMessageButtonProps = {
 export default function SendToMessageButton({
   accessToken,
 }: SendToMessageButtonProps) {
-  const sendToMe = () => {
-    const { Kakao } = window;
-    Kakao.Auth.setAccessToken(accessToken);
-
-    Kakao.API.request({
-      url: "/v2/api/talk/memo/default/send",
-      data: {
-        template_object: {
-          object_type: "text",
-          text: "기본 템플릿으로 제공되는 텍스트 템플릿은 텍스트를 최대 200자까지 표시할 수 있습니다. 텍스트 템플릿은 텍스트 영역과 하나의 기본 버튼을 가집니다. 임의의 버튼을 설정할 수도 있습니다. 여러 장의 이미지, 프로필 정보 등 보다 확장된 형태의 카카오톡 공유는 다른 템플릿을 이용해 보낼 수 있습니다.",
-          link: {
-            mobile_web_url: process.env.NEXT_PUBLIC_BASE_URL,
-            web_url: process.env.NEXT_PUBLIC_BASE_URL,
-          },
+  const sendToMe = async () => {
+    const url = "https://kapi.kakao.com/v2/api/talk/memo/default/send";
+    const data = {
+      template_object: JSON.stringify({
+        object_type: "text",
+        text: "텍스트 영역입니다. 최대 200자 표시 가능합니다.",
+        link: {
+          web_url: process.env.NEXT_PUBLIC_BASE_URL,
+          mobile_web_url: process.env.NEXT_PUBLIC_BASE_URL,
         },
-      },
-    })
-      .then((res: any) => {
-        alert("success: " + JSON.stringify(res));
-      })
-      .catch((err: any) => {
-        alert("error: " + JSON.stringify(err));
+        button_title: "바로 확인",
+      }),
+    };
+
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: new URLSearchParams(data),
       });
+
+      const result = await response.json();
+      if (response.ok) {
+        console.log("Success:", result);
+        alert("메시지 전송 성공: " + JSON.stringify(result));
+      } else {
+        console.error("Error:", result);
+        alert("메시지 전송 실패: " + JSON.stringify(result));
+      }
+    } catch (error) {
+      console.error("Fetch error:", error);
+      alert("Fetch error: " + JSON.stringify(error));
+    }
   };
 
   return (
